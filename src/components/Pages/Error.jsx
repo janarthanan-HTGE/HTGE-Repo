dadc7bc7e7dd3304c5c6ece56a993d73286fee24
Janarthanan-HTGE <janarthananhitech063@gmail.com>
2026-01-22 20:04:44 +0530

    completed home

--- src/components/Pages/Error.jsx
+++ src/components/Pages/Error.jsx
@@ -1,23 +1,27 @@
 import {Link} from 'react-router-dom';
+import NavBar from '../layout/Header';
 
 const ErrorPage = () => {
     return(
-    <section className="error-section mb-5 mt-5 fix">
-      <div className="container">
-        <div className="error-content text-center">
-          <img src="assets/img/404.png" alt="" />
-          <h3 className="fade-up">
-            Page Not Found
-          </h3>
-          <Link
-            to="/"
-            className="theme-btn black-btn mt-2 fade-up"
-          >
-            Back To Home
-          </Link>
+    <>
+      <NavBar/>
+      <section className="error-section mb-5 mt-5 fix">
+        <div className="container">
+          <div className="error-content text-center text-error">
+            <img src="/public/assets/img/coding.png" alt="coding" />
+            <h5 className="fade-up text-error">
+              Website loading… developers arguing over semicolons
+            </h5>
+            <Link
+              to="/"
+              className="theme-btn black-btn mt-2 fade-up"
+            >
+              Back To Home
+            </Link>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
 
     )
 }
