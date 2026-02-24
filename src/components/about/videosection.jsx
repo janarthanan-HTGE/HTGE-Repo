d025f9616ad20e9233658cc3a767469fa45e325e
Janarthanan-HTGE <janarthananhitech063@gmail.com>
2026-02-24 15:15:48 +0530

    xcd

--- src/components/about/videosection.jsx
+++ src/components/about/videosection.jsx
@@ -1,12 +1,16 @@
 import { FaPlay } from "react-icons/fa";
+import useFadeUp from "../../hooks/useFadeUpNor";
+import { useRef } from "react";
 
 const AboutVid = () => {
+    const fadeRef = useRef(null);
+    useFadeUp(fadeRef);
+
     return(
         <section className="video-wrapper video-1 style-2 video-page">
             <div className="container">
                 <div
-                className="video-image bg-cover wow fadeInUp"
-                data-wow-delay="300ms"
+                className="video-image bg-cover fade-up" ref={fadeRef}
                 style={{ backgroundImage: "url(assets/img/video-02.jpg)" }}
                 >
                 <a
--- src/components/service/Serviceprocess.jsx
+++ src/components/service/Serviceprocess.jsx
@@ -4,115 +4,130 @@ import { GrVmMaintenance } from "react-icons/gr";
 import { GoCodeReview } from "react-icons/go";
 import shape6 from "/assets/img/process/shape-6.png";
 import shape7 from "/assets/img/process/shape-7.png";
-
+import { useRef } from "react";
+import useFadeUp from "../../hooks/useFadeUpNor";
 
 const Process = () => {
-    return (
+
+  const ref1 = useRef(null);
+  const ref2 = useRef(null);
+  const ref3 = useRef(null);
+  const ref4 = useRef(null);
+
+  useFadeUp(ref1);
+  useFadeUp(ref2);
+  useFadeUp(ref3);
+  useFadeUp(ref4);
+
+  return (
     <section className="process-wrapper process-2 section-bg p-10">
-        <div className="container">
-            <div className="section-title text-center">
-            <div className="sub-title">
-                <span>HOW IT WORKS</span>
-            </div>
-            <h2 className="split-text right">Our Working Process in 4 Steps</h2>
-            </div>
-
-            <div className="row mt-4">
-            <div
-                className="col-xl-3 col-lg-6 col-md-6 wow fadeInUp"
-                data-wow-delay="200ms"
-            >
-                <div className="signle-process-item pe-xl-4">
-                <img
-                    className="shape-1"
-                    src={shape6}
-                    alt=""
-                />
-                <div className="icons">
-                    <div className="icon-1">
-                    <BiConversation size={40} />
-                    </div>
-                    <div className="icon-2">01</div>
-                </div>
-                <h4 className="mb-2" style={{fontWeight:"500"}}>Project Discussion</h4>
-                <p>
-                    Accelerate innovation with world-class tech teams We’ll match you
-                    to an entire remote team.
-                </p>
-                </div>
-            </div>
-
-            <div
-                className="col-xl-3 col-lg-6 col-md-6 mt-xl-5 wow fadeInUp"
-                data-wow-delay="400ms"
-            >
-                <div className="signle-process-item pe-xl-3 ps-xl-2">
-                <img
-                    className="shape-2"
-                    src={shape7}
-                    alt=""
-                />
-                <div className="icons">
-                    <div className="icon-1">
-                    <LuMonitorCheck size={40} />
-                    </div>
-                    <div className="icon-2">02</div>
-                </div>
-                <h4 className="mb-2" style={{fontWeight:"500"}}>Testing & Trying</h4>
-                <p>
-                    Accelerate innovation with world-class tech teams We’ll match you
-                    to an entire remote team.
-                </p>
-                </div>
-            </div>
-
-            <div
-                className="col-xl-3 col-lg-6 col-md-6 wow fadeInUp"
-                data-wow-delay="600ms"
-            >
-                <div className="signle-process-item ps-xl-3 pe-xl-2">
-                <img
-                    className="shape-1"
-                    src={shape6}
-                    alt=""
-                />
-                <div className="icons">
-                    <div className="icon-1">
-                    <GrVmMaintenance size={40} />
-                    </div>
-                    <div className="icon-2">03</div>
-                </div>
-                <h4 className="mb-2" style={{fontWeight:"500"}}>Execute & Install</h4>
-                <p>
-                    Accelerate innovation with world-class tech teams We’ll match you
-                    to an entire remote team.
-                </p>
-                </div>
-            </div>
-
-            <div
-                className="col-xl-3 col-lg-6 col-md-6 mt-xl-5 wow fadeInUp"
-                data-wow-delay="400ms"
-            >
-                <div className="signle-process-item ps-xl-4">
-                <div className="icons">
-                    <div className="icon-1">
-                    <GoCodeReview size={40} />
-                    </div>
-                    <div className="icon-2">04</div>
-                </div>
-                <h4 className="mb-2" style={{fontWeight:"500"}}>Review & Fixing</h4>
-                <p>
-                    Accelerate innovation with world-class tech teams We’ll match you
-                    to an entire remote team.
-                </p>
-                </div>
-            </div>
-            </div>
+      <div className="container">
+        <div className="section-title text-center">
+          <div className="sub-title">
+            <span>HOW IT WORKS</span>
+          </div>
+          <h2 className="split-text right">
+            Our Working Process in 4 Steps
+          </h2>
         </div>
-        </section>
 
-    )
-}
+        <div className="row mt-4">
+
+          {/* 01 */}
+          <div className="col-xl-3 col-lg-6 col-md-6">
+            <div
+              ref={ref1}
+              className="signle-process-item pe-xl-4 fade-up"
+              style={{ "--delay": "0.2s" }}
+            >
+              <img className="shape-1" src={shape6} alt="" />
+              <div className="icons">
+                <div className="icon-1">
+                  <BiConversation size={40} />
+                </div>
+                <div className="icon-2">01</div>
+              </div>
+              <h4 className="mb-2" style={{ fontWeight: "500" }}>
+                Requirement Analysis
+              </h4>
+              <p>
+                Understand client needs, business goals, target audience, and define clear project scope and technical requirements.
+              </p>
+            </div>
+          </div>
+
+          {/* 02 */}
+          <div className="col-xl-3 col-lg-6 col-md-6 mt-xl-5">
+            <div
+              ref={ref2}
+              className="signle-process-item pe-xl-3 ps-xl-2 fade-up"
+              style={{ "--delay": "0.4s" }}
+            >
+              <img className="shape-2" src={shape7} alt="" />
+              <div className="icons">
+                <div className="icon-1">
+                  <LuMonitorCheck size={40} />
+                </div>
+                <div className="icon-2">02</div>
+              </div>
+              <h4 className="mb-2" style={{ fontWeight: "500" }}>
+                Planning & Strategy
+              </h4>
+              <p>
+                Create project roadmap, choose technologies, design wireframes, and finalize development timeline and milestones.
+              </p>
+            </div>
+          </div>
+
+          {/* 03 */}
+          <div className="col-xl-3 col-lg-6 col-md-6">
+            <div
+              ref={ref3}
+              className="signle-process-item ps-xl-3 pe-xl-2 fade-up"
+              style={{ "--delay": "0.6s" }}
+            >
+              <img className="shape-1" src={shape6} alt="" />
+              <div className="icons">
+                <div className="icon-1">
+                  <GrVmMaintenance size={40} />
+                </div>
+                <div className="icon-2">03</div>
+              </div>
+              <h4 className="mb-2" style={{ fontWeight: "500" }}>
+                Development & Testing
+              </h4>
+              <p>
+                Build the solution, integrate features, perform quality testing, and ensure security and performance.
+              </p>
+            </div>
+          </div>
+
+          {/* 04 */}
+          <div className="col-xl-3 col-lg-6 col-md-6 mt-xl-5">
+            <div
+              ref={ref4}
+              className="signle-process-item ps-xl-4 fade-up"
+              style={{ "--delay": "0.8s" }}
+            >
+              <div className="icons">
+                <div className="icon-1">
+                  <GoCodeReview size={40} />
+                </div>
+                <div className="icon-2">04</div>
+              </div>
+              <h4 className="mb-2" style={{ fontWeight: "500" }}>
+                Deployment & Support
+              </h4>
+              <p>
+                Deploy the solution to production, provide ongoing support, and ensure smooth operation and maintenance.
+              </p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Process;
